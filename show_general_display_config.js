550dce3a2663c6180e3653a8286bb1c593155dfc
Andrew L <andy.ol@free.fr>
2015-09-11 17:39:21 +0200

    Update show_general_display_config.js
    New output to window mode (WIP)

--- show_general_display_config.js
+++ show_general_display_config.js
@@ -1,27 +1,73 @@
-var wiW = window.innerWidth; 
-var wiH = window.innerHeight;
-var woW = window.outerWidth;
-var woH = window.outerHeight;
-var avW = screen.availWidth;
-var avH = screen.availHeight;
-var scW = screen.width;
-var scH = screen.height;
-var clD = screen.colorDepth;
-var clR = screen.pixelDepth;
+/*  webdesign_ruler.js
+ *
+ *  A little script to "alert" useful measures (screen, window...)
+ *
+ *  Written with love by Andrew-L-777 @GitHub (still WIP)
+ */
+ 
+/* SETTINGS */
+var isAlerting = true;
+var isWindowOutputOn = true;
 
-alert(
-  "Window (inner):\t\t" +
-  "W=" + wiW + " " + "H=" + wiH + "\n"
-+
-  "Window (outer):\t\t" +
-  "W=" + woW + " " + "H=" + woH + "\n"
-+
-  "Screen resolution:\t\t" +
-  "W=" + scW + " " + "H=" + scH + "\n"
-+
-  "Screen (available):\t\t" +
-  "W=" + avW + " " + "H=" + avH + "\n"
-+
-  "Colors (depth, resol.):\t\t" +
-  clD + "bits, " + clR + "bits"
-);
+/* VARIABLES */
+var winName = window.name;
+var winURL  = window.location.href;
+var winIn   = {w: window.innerWidth, h: window.innerHeight};
+var winOut  = {w: window.outerWidth, h: window.outerHeight};
+var winPos  = {x: window.screenX, y: window.screenY};
+var avail   = {w: screen.availWidth, h: screen.availHeight};
+var resol   = {w: screen.width, h: screen.height};
+var colorDepth = screen.colorDepth;
+
+var outputWindow;
+var date_init = new Date();
+var date_time = date_init.getDate() + "/" + date_init.getMonth() + "/" + date_init.getDate() + " " +
+	date_init.getHours() + ":" + date_init.getMinutes() + ":" + date_init.getSeconds();
+var window_att = {u: "", n: "Webdesign ruler output - " + date_time, s: "width=800, height=800", r: ""};
+
+if (winName == "")
+	winName = "[No name]";
+
+if (isAlerting)
+	alert(
+	  "___WINDOW ATTRIBUTES____________" + "\n" +
+	  "Focused:\t\t" + winName + "\n" +
+	  "URL:\t" + winURL + "\n"
+	+
+	  "Window (inner):\t\t" +
+	  "W=" + winIn.w + " " + "H=" + winIn.h + "\n"
+	+
+	  "Window (outer):\t\t" +
+	  "W=" + winOut.w + " " + "H=" + winOut.h + "\n"
+	+
+	  "Window position:\t\t" +
+	  "X=" + winPos.x + " " + "Y=" + winPos.y + "\n"
+	+ "\n" +
+		
+	  "___SCREEN ATTRIBUTES______" + "\n"
+	+
+	  "Screen resolution:\t\t" +
+	  "W=" + resol.w + " " + "H=" + resol.h + "\n"
+	+
+	  "Screen (available):\t\t" +
+	  "W=" + avail.w + " " + "H=" + avail.h + "\n"
+	+
+	  "Color depth:\t\t\t" + colorDepth + "bits"
+	);
+
+// TODO
+/*
+if (isWindowOutputOn) {
+	outputWindow = window.open(window_att.u, window_att.n, window_att.s, window_att.r);
+	document.write("<!DOCTYPE html>" +
+	"<html>" +
+	"<head>" +
+		"<title>Title of the document</title>" +
+	"</head>" +
+	"<body>" +
+		"<p>Test</p>" +
+	"</body>" +
+	"</html>"
+	);
+}
+*/
